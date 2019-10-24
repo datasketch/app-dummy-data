b2613edb4cc47bd98044d95870cf19c144d3b5fc
David Daza <03dazal@gmail.com>
2019-10-24 14:34:30 -0500

    add functions generate countries and emails

--- app.R
+++ app.R
@@ -1,12 +1,46 @@
 library(shiny)
 library(charlatan)
 
+# Function to generate city names
+ch_city <- function(n = 1, locale = NULL) {
+  if (n == 1) {
+    AddressProvider$new(locale = locale)$city()
+  } else {
+    x <- AddressProvider$new(locale = locale)
+    replicate(n, x$city())
+  }
+}
+
+# Function to generate emails
+ch_email <- function(n = 1, locale = NULL) {
+  if (n == 1) {
+    InternetProvider$new(locale = locale)$email()
+  } else {
+    x <- InternetProvider$new(locale = locale)
+    replicate(n, x$email())
+  }
+}
+
+# Function to create table with variables outside generic ch_generate
+genTable <- function(variables, rows) {
+  cols <- stats::setNames(
+    lapply(variables, function(var) {
+      fun <- eval(parse(text = paste0("ch_", var)))
+      fun(rows)
+    }),
+    variables
+  )
+  tibble::as_tibble(cols)
+}
+
 variables <- c(
   'Name' = 'name',
   'Job Title' = 'job',
   'Phone Number' = 'phone_number',
-  'Currency' = 'currency'
+  'Email' = 'email',
+  'City' = 'city'
 )
+  
 
 ui <- fluidPage(
   titlePanel('fakeR: Generate fake data'),
@@ -33,7 +67,7 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   output$fakedata <- renderTable({
-    ch_generate(input$variables, n = input$rows)
+    genTable(input$variables, rows = input$rows)
   })
 }
 
